30ec7a5cdca9829c0bec02c824ad186bfbb2b9f6
gaetanbo <32732112+gaetanbo@users.noreply.github.com>
2019-05-19 17:26:01 +0200

    added select for blackbiz

--- blackbiz.js
+++ blackbiz.js
@@ -1,17 +1,52 @@
+//						#black_biz_result
 $(document).ready(function(){
-	console.log('bbiz !');
-	blackbiz();
-	//		#black_biz_result
-  	//	    https://cors.io/?
-  	// 		https://gameinfo.albiononline.com/api/gameinfo/items/T2_BAG
+
+	$("#select_kind").change(function(){
+		let kindAsked = $("#select_kind").children("option:selected").val();
+		let jsonDoc = "ressources/"+ressourceAsked+".json";
+		$("#select_kind_type").empty();
+		$.getJSON(jsonDoc,function(data) {
+				$.each(data, function(key,val) {
+						var tierRessourceAsked = val.UniqueName;
+						var o = new Option(val.UniqueName,val.UniqueName);
+						$(o).html(val.UniqueName);
+						$("#select_kind_type").append(o);
+				});
+		});
+		blackbiz();
+	});
+
  	function blackbiz1 () {
-		$.get("https://cors.io/?https://gameinfo.albiononline.com/api/gameinfo/items/T2_BAG/data",function(d3){
-      		console.log(d3);
-      		//console.log(d3.localizedDescriptions["FR-FR"]); 
-    	});
-  	}
+		console.log('bbiz !');
+	};
+
+
 });
 
+
+	let price_min;
+	let dataprice;
+	function fetchData (item, enchantLevel = 0,location = 0,quality = 0){
+		// location, enchantLevel et quality peuvent etre nul
+		var request = "https://www.albion-online-data.com/api/v2/stats/prices/"+item;
+			if (enchantLevel) {
+				request += enchantLevel;
+			} else {
+			 // console.log('no enchantLevelAsked');
+			}
+			if (location) {
+				request +="?locations=" +location;
+			} else {
+				// console.log('no locationAsked');
+			}
+			$.get(request, function(dataprice){
+				// price_min = dataprice[0].sell_price_min;
+				//        This only return the first city in the array, rarely the same city
+				// console.log("price min for "+item+"= "+price_min);
+				// console.log(dataprice);
+				return dataprice;
+			});
+	};
 
 	// IF PRICE ARE MAX BENEF , ADD CLASSES  to rd :
 	// 	'table-danger'		RED
@@ -19,10 +54,9 @@ $(document).ready(function(){
 	// 	'table-sucess'		green
 	// 	'table-info'		blue
 
-
-	//	#itemCategory
+  //	    https://cors.io/?
+	//			#itemCategory
 	//			https://gameinfo.albiononline.com/api/gameinfo/events/33204877
 	// Item Data:		https://gameinfo.albiononline.com/api/gameinfo/items/T2_BAG/data/
 	// item category : 	https://gameinfo.albiononline.com/api/gameinfo/items/_itemCategoryTree
 	// item icon : 		https://gameinfo.albiononline.com/api/gameinfo/items/T2_BAG/
- 
