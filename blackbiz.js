aa876b13785a64226025ea5615e8edf5453ab77f
Guyb <gaetan987@hotmail.fr>
2019-05-20 00:49:58 +0200

    fcuked all up,changing strategy -rly need to improve getprice func

--- blackbiz.js
+++ blackbiz.js
@@ -3,21 +3,27 @@ let pricessss;
 let price_min_asked;
 $(document).ready(function(){
 
+	$("#select_kind").change(function(){ 
+    	shakelastselect();
+    });
     $("#select_kind_type").change(function(){ 
-    	gotgettem();
+    	gotgettem2();
     });
 	$("#go").click(function() {
    		let itemAsked = $("#select_equipment").children("option:selected").val();
-   		console.log(itemAsked);
    		var city = "Caerleon";
 		getprice(itemAsked,null,city);
-		console.log(price_min_asked);
 
 	});
     $("#select_equipment").change(function(){
-   		console.log('questcequonattnedspourlesprix');
+   		//	console.log('questcequonattnedspourlesprix');
     });
-
+// Armure de gardetombe de l'adepte
+// Armure de judicateur de l'adepte
+// Chaussures de mercenaire de l'adepte
+// Chaussures de tourmenteur de l'adepte
+// Capuche de tourmenteur de l'adepte
+// Veste de mercenaire de l'adepte
 	function gotgettem(){
         $("#select_equipment").empty();
         let kindAsked = $("#select_kind").children("option:selected").val();
@@ -56,8 +62,48 @@ $(document).ready(function(){
         });
     };
 
+	function gotgettem2(){
+	    $("#resultblackbiz").empty();
+	    let kindAsked = $("#select_kind").children("option:selected").val();
+	    let kindTypeAsked = $("#select_kind_type").children("option:selected").val();
+	    let jsonDoc2 = "ressources/"+kindAsked+".json";
+	    $.getJSON(jsonDoc2,function(data) {
+	        $.each(data, function(key,val) {
+	          	if(val.UniqueName.indexOf("T1_") >= 0 ) {
+	                if ( val.UniqueName.indexOf("T2_") >= 0) {
+	            		if (val.UniqueName.indexOf("T3_") >= 0 ) {
+	            		}
+	            	}
+	            } else {
+	                    if(val.UniqueName.indexOf("@") >= 0){
+	                    	//console.log(val.UniqueName + ' --> @');
+	                    	return null;
+	                    } else {
+	                    	if (val.UniqueName.indexOf("T4_") >= 0) {
+	                       		if (val.UniqueName.indexOf(kindTypeAsked) >= 0 ) {
+	                              	//var nom_fr = (val.LocalizedNames[2].Value).substring(0,25);
+	                              	//var nom_fr = (val.LocalizedNames[2].Value).slice(12);
+	                              	var nom_fr = val.LocalizedNames[2].Value.substring(0, val.LocalizedNames[2].Value.length-12);
 
+	                                //optionselectbbiz
+	                                $("#resultblackbiz").append("<tr><td>"+nom_fr+"</td><td></td><td></td><td></td><td></td><td></td>");
+	                       		} else {
+	                       			return null;
+	                       		}  
+	                    	} else {
+	                    		// console.log(val.UniqueName + ' --> dommage');
+	                    		return null;
+	                    	} 
+	                    }
+	            }
+	        });
+	        console.log('fini');
+	    });
+	};
 
+	function shakelastselect() {
+		$("#select_kind_type").effect("shake", {direction:"up"});
+	};
     function getprice(item, enchantLevel = 0,location = 0,quality = 0) {
     // location, enchantLevel et quality peuvent etre nul
     var request = "https://www.albion-online-data.com/api/v2/stats/prices/"+item;
