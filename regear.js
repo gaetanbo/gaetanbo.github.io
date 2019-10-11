e11e582f16174751c8e75f1dc4b649785ab3e83f
Guyb <gaetan987@hotmail.fr>
2019-10-11 15:20:45 +0200

    update regear

--- regear.js
+++ regear.js
@@ -3,8 +3,28 @@
 $(document).ready(function(){ 
     $("#fm_test").click(function(){
         console.log('test');
+//        $("#select_ressource_type").empty();
+//        $.getJSON(jsonDoc,function(data) {
+//             $.each(data, function(key,val) {
+//                 var tierRessourceAsked = val.UniqueName;
+//                 var o = new Option(val.UniqueName,val.UniqueName);
+//                 $(o).html(val.UniqueName);
+//                 $("#select_ressource_type").append(o);
+//             });
+//         });
     });
     $("#regear").click(function(){
+        $("#fm_cape").empty();
+        $("#fm_head").empty();
+        $("#fm_bag").empty();
+        $("#fm_main").empty();
+        $("#fm_torso").empty();
+        $("#fm_boot").empty();
+        $("#fm_food").empty();
+        $("#fm_potion").empty();
+        $("#fm_mount").empty();
+
+        let total = 0;
         // gears : T4_BAG, T6_HEAD_LEATHER_SET3, T4_CAPEITEM_FW_LYMHURST,T6_2H_INFERNOSTAFF,T6_ARMOR_CLOTH_SET1,T6_SHOES_CLOTH_SET1 ,T7_MEAL_OMELETTE,T4_POTION_COOLDOWN,T5_MOUNT_ARMORED_HORSE
         $.get("https://www.albion-online-data.com/api/v2/stats/prices/T4_BAG?locations=Martlock",function(d3){
             //console.log(d3);
@@ -40,7 +60,7 @@ $(document).ready(function(){
         });
         $.get("https://www.albion-online-data.com/api/v2/stats/prices/T6_SHOES_CLOTH_SET1?locations=Martlock",function(d3){
             let price_min = d3[0].sell_price_min;
-            let city = d3[0].city;
+            let city = d3[0].city;            
             $("#fm_boot").append(numberWithCommas(price_min) + ' at ' + city);
         });
         $.get("https://www.albion-online-data.com/api/v2/stats/prices/T4_POTION_COOLDOWN?locations=Martlock",function(d3){
