3242ebd2cd53ebd392b07e47d3622ad9271e84a8
Guyb <gaetan987@hotmail.fr>
2019-10-28 22:13:22 +0100

    tweak css a bit

--- regear.js
+++ regear.js
@@ -1,10 +1,7 @@
 
 $(document).ready(function(){ 
 
-
-
-    $("#regear_pul_prices").click(function(){
-            
+    $("#regear_pul_prices").click(function(){ 
         let gear_set = $("#regear_select_gearset").children("option:selected").val();
         switch (gear_set) {
             case 'tank':
@@ -20,7 +17,6 @@ $(document).ready(function(){
                 var gears = ['T6_BAG','T8_HEAD_LEATHER_SET1','T4_CAPEITEM_FW_FORTSTERLING','T8_2H_HOLYSTAFF_HELL','T8_ARMOR_CLOTH_SET2','T5_MOUNT_ARMORED_HORSE','T8_SHOES_CLOTH_SET1','T7_MEAL_OMELETTE','T4_POTION_COOLDOWN'];
                 break;
         }
-        // var cities = {"1" : "Bridgewatch","2" : "Caerleon","3" : "Fort Sterling","5" : "Lymhurst","6" : "Martlock","10" : "Thetford"};
         $("#regear_result").empty();
         let city = $("#regear_select_city").children("option:selected").val();
         let gear_total_cost = 0;
@@ -29,25 +25,27 @@ $(document).ready(function(){
                 let dirty_name = d3[0].item_id;
                 let name = dirty_name.replace(/_/g, " ");
                 let dirty_price_min = d3[0].sell_price_min;
-                let price_min = numberWithCommas(dirty_price_min);
+                gear_total_cost = gear_total_cost + dirty_price_min;
+                AddTotal(numberWithCommas(gear_total_cost));
+                //console.log("TOTAL GEAR COST : "+numberWithCommas(gear_total_cost));
+                let price_min = numberWithCommas(d3[0].sell_price_min);
                 let price_date = d3[0].sell_price_min_date;
                 let price_date_in_second = Date.parse(price_date);
                 let date_now = Date.now();
                 if ( (date_now - price_date_in_second) > 42000000) { // si la date reçu est supérieure à 12h from now
-                    $("#regear_result").append("<div class=\"regear-sub-container\"><img class=\"img_logo\" src=\"https://gameinfo.albiononline.com/api/gameinfo/items/"+element+".png\"><p><u class=\"undertext\">"+name+"</u></p><p><font color=\"red\"> "+price_min+"</font> silver at "+city+"</p></div>");
+                    $("#regear_result").append("<div class=\"regear-sub-container\"><img class=\"img_logo\" src=\"https://gameinfo.albiononline.com/api/gameinfo/items/"+element+".png\"><p><u class=\"undertext\">"+name+"</u></p><p><font color=\"red\">  "+price_min+"</font> silver at "+city+"</p></div>");
                 } else{
-                    $("#regear_result").append("<div class=\"regear-sub-container\"><img class=\"img_logo\" src=\"https://gameinfo.albiononline.com/api/gameinfo/items/"+element+".png\"><p><u class=\"undertext\">"+name+"</u></p><p><font color=\"green\"> "+price_min+"</font> silver at "+city+"</p></div>");
-                }
-                gear_total_cost = gear_total_cost + dirty_price_min;
-                console.log("TOTAL GEAR COST : "+numberWithCommas(gear_total_cost));
+                    $("#regear_result").append("<div class=\"regear-sub-container\"><img class=\"img_logo\" src=\"https://gameinfo.albiononline.com/api/gameinfo/items/"+element+".png\"><p><u class=\"undertext\">"+name+"</u></p><p> "+price_min+" silver at "+city+"</p></div>");
+                }               
             });
-            //$("#regear_result").append("<p>Total : "+gear_total_cost);        
         });
     });
 });
 
 
-
+function AddTotal(number){
+    console.log('TOTAL ' +number);
+}
 function numberWithCommas(number) {
     var parts = number.toString().split(".");
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
