2c92016b1b4cf3bc3f43ede29b1b7ca7c0ccec63
Guyb <gaetan987@hotmail.fr>
2019-10-28 22:35:39 +0100

    Fixed French on Bbiz

--- bbiz.js
+++ bbiz.js
@@ -15,28 +15,20 @@ $(document).ready(function(){
 		let jsonDoc2 = "ressources/"+kindAsked+".json";
 	    $.getJSON(jsonDoc2,function(data) { 	
 			$.each(data, function(key,val) {
-	    		var item = val.UniqueName;
-	    		var itemLocalName = val.LocalizedNames[2].Value;
+				var item = val.UniqueName;
+	    		var itemLocalName = val.LocalizedNames[0].Value;
 	    		if ( item.includes("@1") ) {
-	    			//item is .1
-	    			// console.log(".1");
-	    			var enchant = ".1";
+	    			var enchant = "1";
 	    		} else if ( item.includes("@2") ) {
-	    			//item is .2
-	    			// console.log(".2");
-	    			var enchant = ".2";
+	    			var enchant = "2";
 	    		} else if ( item.includes("@3") ) {
-	    			//item is .3
-	    			// console.log(".3");
-	    			var enchant = ".3";
+	    			var enchant = "3";
 	    		} else {
-	    			// item is .0
-	    			// console.log("flat");
 	    			var enchant = "flat";
 	    		}
 	    		var profitWanted = $("#profit_wanted").children("option:selected").val();
 	    			var q_level = [1,2,3,4,5];
-	    			var q_text = ["None", "Normale","Acceptable","Admirable","Formidable","Exceptionnelle"];
+	    			var q_text = ["None", "Normal","Good","Outstanding","Excellent","Masterpiece"];
 	    			let Ca_prices = [];let BM_prices = [];let diffs = []; 
 	    		var request = "https://www.albion-online-data.com/api/v2/stats/prices/"+item+"?locations=Caerleon,BlackMarket";
 	    		$.get(request,function(apiPrice) {
@@ -55,7 +47,8 @@ $(document).ready(function(){
 	    				q_level.forEach(q=> {
 	    					if(BM_prices[q] && Ca_prices[q] && diffs[q] && diffs[q] > profitWanted) {
 								$(".lds-ring").remove();
-		    					$("#resultblackbiz").append("<tr><td><img width=\"64\" height=\"64\" title=" + item + " src=" + "  https://gameinfo.albiononline.com/api/gameinfo/items/"+ item + "?quality="+q+"></img>"+itemLocalName+"</td><td> Enchant : "+enchant +"<br> Quality : "+ q_text[q]+"</td><td>"+BM_prices[q]+"</td><td>"+Ca_prices[q]+"</td><td style=" + (diffs[q]>0?'color:green':'color:red') + ">"+diffs[q]+"</td></tr>");
+		    					$("#resultblackbiz").append("<tr><td><img width=\"64\" height=\"64\" title=" + item + " src=" + "  https://gameinfo.albiononline.com/api/gameinfo/items/"+ item + "?quality="+q+"></img>"+itemLocalName+"</td><td><span class=\"enchant"+enchant+"\"> Enchant : "+enchant +"</span><br> Quality : "+ q_text[q]+"</td><td>"+BM_prices[q]+"</td><td>"+Ca_prices[q]+"</td><td style=" + (diffs[q]>0?'color:green':'color:red') + ">"+diffs[q]+"</td></tr>");
+		    					//$("#resultblackbiz").append("<tr><td><img width=\"64\" height=\"64\" title=" + item + " src=" + "  https://gameinfo.albiononline.com/api/gameinfo/items/"+ item + "?quality="+q+"></img>"+itemLocalName+"</td><td> Enchant : "+enchant +"<br> Quality : "+ q_text[q]+"</td><td>"+BM_prices[q]+"</td><td>"+Ca_prices[q]+"</td><td style=" + (diffs[q]>0?'color:green':'color:red') + ">"+diffs[q]+"</td></tr>");
 							}
 							else{
 								//$(".lds-ring").remove();
